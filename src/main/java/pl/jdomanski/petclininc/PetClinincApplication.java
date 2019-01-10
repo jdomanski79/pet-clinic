18d15865923dd4b0925bf87602640cd21f195452
jarek <jarek@laptop>
2019-01-10 20:43:14 +0100

    initial commit

---
+++ src/main/java/pl/jdomanski/petclininc/PetClinincApplication.java
@@ -0,0 +1,14 @@
+package pl.jdomanski.petclininc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinincApplication.class, args);
+	}
+
+}
+
