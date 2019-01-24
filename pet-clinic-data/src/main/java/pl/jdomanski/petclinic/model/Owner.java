c7d582465f6966a5267be4c1a926244b5b660052
Jarek <jarek@jdomanski79.nazwa.pl>
2019-01-24 21:40:04 +0100

    Added visits, updated owners with pet relationship, closes #25

--- pet-clinic-data/src/main/java/pl/jdomanski/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/jdomanski/petclinic/model/Owner.java
@@ -1,5 +1,18 @@
 package pl.jdomanski.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person{
+	
+	Set<Pet> pets = new HashSet<>();
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 
 }
